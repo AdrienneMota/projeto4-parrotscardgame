64111a199de7e1056b2253ff4316b70518b73c30
Adrienne Mota <adriennemota97@gmail.com>
2022-08-19 20:07:33 -0400

    função selecionar cartas completa

--- script.js
+++ script.js
@@ -1,9 +1,57 @@
-    let qtdcards = prompt('Com quantas cartas você deseja jogar?');
-    
-    while((qtdcards%2 !== 0) && (qtdcards > 4 || qtdcards < 14)){
-        qtdcards = prompt('Digite um valor par menor ou igual a 14.')
+    let qtdcards = parseInt(prompt('Com quantas cartas você deseja jogar?'));
+        
+    while((qtdcards < 4 || qtdcards>14) || (qtdcards%2 != 0 )  || (qtdcards == undefined)){
+        qtdcards = prompt('Digite um valor numérico par entre 4 e 14.');
     }
 
-    alert('você pode jogar!')
-    
+    mostrarcartas();
 
+    function mostrarcartas(){
+        const jogo = document.querySelector('.container');
+        jogo.classList.remove('esconder');  
+        
+        //embaralhar as cartas
+        let cartas = [ 
+            './img/bobrossparrot.gif',
+            './img/explodyparrot.gif',
+            './img/fiestaparrot.gif',
+            './img/metalparrot.gif',
+            './img/revertitparrot.gif',
+            './img/tripletsparrot.gif',
+            '/img/unicornparrot.gif'
+        ];
+        
+        embaralhar(cartas);
+
+        //selecionar cartas
+
+        let cartasselecionas = [];
+        let copiacarta = [];
+        let cartasjogo = [];
+
+        for(let i = 0; i< (qtdcards/2); i++){
+            cartasselecionas.push(cartas[i]);
+            copiacarta.push(cartas[i]);
+        }
+
+        cartasjogo = cartasselecionas.concat(copiacarta);
+
+        console.log(embaralhar(cartasjogo));
+
+        
+
+    }
+
+    function embaralhar(array){
+        array.sort(comparador);
+        function comparador() { 
+            return Math.random() - 0.5; 
+        }
+    }
+
+
+
+
+
+    
+   
